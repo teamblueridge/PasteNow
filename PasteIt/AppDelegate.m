6fda18c0987d01b530fafb3ce7ad49575069bd80
Simon Sickle <simon@simonsickle.com>
2015-12-04 08:34:05 -0600

    Fix bug where history button wouldn't show

--- PasteIt/AppDelegate.m
+++ PasteIt/AppDelegate.m
@@ -19,8 +19,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
     splitViewController.delegate = self;
     return YES;
 }
